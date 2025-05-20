80eb53005f36535ad40cedf87d7319ca6e884d76
ギガ <115336867+dev0gig@users.noreply.github.com>
2025-05-20 06:56:03 +0000

    new tab with contacts

---
+++ unicorn/we-iframe/we-contactsDATA.js
@@ -0,0 +1,44 @@
+// kontakte-data.js
+
+const kontakte = [
+    {
+        id: 1,
+        name: "ISTA",
+        nummer: "050 230 230 300",
+        adresse: "-",
+        mail: "info@ista.at",
+        notizen: ""
+    },
+    {
+        id: 2,
+        name: "Thermomatic",
+        nummer: "01 712 63 35",
+        adresse: "-",
+        mail: "we@thermomatic.at",
+        notizen: ""
+    },
+    {
+        id: 3,
+        name: "Fernablesung",
+        nummer: "01 4004 84555",
+        adresse: "-",
+        mail: "-",
+        notizen: "Erreichbar von MO-DO 07:30 - 16:00 Uhr, FR 07:30 - 12:00 Uhr\n(EDW - Energiedienstleistungen wirtschaftlicher Anlagenbetrieb)"
+    },
+    {
+        id: 4,
+        name: "Schönherr",
+        nummer: "+43 1 53437 50522",
+        adresse: "-",
+        mail: "fww@schoenherr.eu",
+        notizen: ""
+    },
+    {
+        id: 5,
+        name: "Störungsdienst FW",
+        nummer: "0800 500 751",
+        adresse: "-",
+        mail: "technischerkundendienst@wienenergie.at",
+        notizen: "01 4004 84830 - direkte nummer"
+    },
+];
